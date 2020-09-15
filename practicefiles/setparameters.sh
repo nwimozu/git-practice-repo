d1f70546f33510bb635026313efcbc3690e62a03
yespatel83 <yespatel@chapman.edu>
2020-09-15 15:04:07 -0700

    test_push

---
+++ practicefiles/setparameters.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Separate main parameter file into three files
+# cut -f 1 allpara.txt > Wo.txt
+cut -f 2 allpara.txt > pamp.txt
+cut -f 3 allpara.txt > Freq.txt
+
+# Count number of lines in files
+numlines=$(grep -c "^" allpara.txt)
+
+# initialize variables
+pamp=0
+Freq=0
+# For loop that will write files
+for i in `seq 1 $numlines`;
+do
+# Sets Wo based on i
+pamp=$(awk -v var="$i" 'NR==var' pamp.txt)
+# Writes file to replace line with Wo
+cat template-parameters |  awk -v var="$pamp" 'NR==6 {$0="pamp = "'"var"'"  \\ "} 1' template-parameters > parametersw${i}
+# Sets Freq based on i
+Freq=$(awk -v var="$i" 'NR==var' Freq.txt)
+# Writes file to replace line with Freq
+cat template-parameters |  awk -v var="$Freq" 'NR==7 {$0="freq = "'"var"'"   \\"} 1' parametersw${i} > parametersw${i}f${i}
+# cat template-parameters |  awk -v var="$Freq" 'NR==8 {$0="iposn = "'"var"'"   \\"} 1' parametersw${i}f${i} > parametersw${i}f${i}l${i}
+# Cleans up folder
+# mv parametersw${i}f${i}l${i} parameters${i}
+mv parametersw${i}f${i} parameters${i}
+rm parametersw${i} 
+
+done
